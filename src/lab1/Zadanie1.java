31c112bcd2d29c1945a2676e4178011f733fa248
cotyniepowiesz <50676544+cotyniepowiesz@users.noreply.github.com>
2019-10-22 18:46:32 +0200

    First commit

---
+++ src/lab1/Zadanie1.java
@@ -0,0 +1,36 @@
+package lab1;
+
+public class Zadanie1
+{
+	public static void main(String[] args) 
+	{
+		int wysokosc = 15;
+		for (int i = 0; i < wysokosc+1; i++)
+			System.out.print(" ");
+		System.out.println("$");
+		for (int i = 0; i < wysokosc; i++)
+		{
+			for (int k = wysokosc; k > i; k--)
+				System.out.print(" ");
+			System.out.print("$");
+			for (int j = 0; j < i+1; j++)
+			{				
+				System.out.print("*");
+				if (j!=i)
+				{
+					if ((j+1)%3!=0)
+						System.out.print(" ");
+					else
+						System.out.print("$");
+				}	
+			}		
+			System.out.println("$");
+		}
+		for (int i = 0; i < (wysokosc+1)*2; i++)
+			System.out.print("$");
+		System.out.println("$");
+		for (int i = 0; i < wysokosc+1; i++)
+			System.out.print(" ");
+		System.out.print("#");
+	}
+}
