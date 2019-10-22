f69a1a7bf87a4a091f9da777d53f37cd28106afb
cotyniepowiesz <50676544+cotyniepowiesz@users.noreply.github.com>
2019-10-22 18:56:13 +0200

    c

---
+++ src/lab2/Zadanie3.java
@@ -0,0 +1,15 @@
+package lab2;
+import java.lang.Math.*;
+import java.util.Scanner;
+
+public class Zadanie3 {
+	public static void main (String[] args)
+	{
+		Scanner keyboard = new Scanner(System.in);
+		System.out.println("Podaj predkosc w mph: ");
+		double mph = keyboard.nextDouble();
+		double kmh = mph * 1.61;
+		System.out.print("Predkosc w km/h wynosi: "+kmh);
+		keyboard.close();
+	}
+}
