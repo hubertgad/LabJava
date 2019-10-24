7de64baea6ba09d31d1629776f3aa208febee125
Hubert Gad <50676544+hubertgad@users.noreply.github.com>
2019-10-24 14:08:08 +0200

    Update Zadanie6.java

--- src/lab2/Zadanie6.java
+++ src/lab2/Zadanie6.java
@@ -1,1 +1,4 @@
 
+6. W salonie samochodowym zaoferowano sprzedaż leasingową pojazdów. Napisz kod, który policzy miesięczne zobowiązanie z uwzględnieniem odsetek i wpłaconego kapitału własnego.
+Dane wejściowe to maksymalna kwota: 300 tyś PLN, liczba rat do 96. Oprocentowanie wynosi:
+do 24 miesięcy 2% do 48 miesięcy 3% do 60 miesięcy 4% do 72 miesięcy 5% do 96 miesięcy 6%
