c478f27a85cd9afd7a523fafe425ba4b17ba4810
Hubert Gad <hubertgad@gmail.com>
2019-10-27 22:12:24 +0100

    dodanie nowych funkcji

--- src/lab3/Zadanie1.java
+++ src/lab3/Zadanie1.java
@@ -9,7 +9,7 @@ import java.util.Scanner;
 
 public class Zadanie1 
 {
-	public static void main(String[] args) 
+	public static void main(String[] args)
 	{
 		Scanner 
 			keyboard = new Scanner(System.in);
