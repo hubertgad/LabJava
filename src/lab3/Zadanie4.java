6d77031f86ffa156c7dcdde0dbde8db2f3f47853
Hubert Gad <50676544+hubertgad@users.noreply.github.com>
2019-10-30 13:27:45 +0100

    Update Zadanie4.java

--- src/lab3/Zadanie4.java
+++ src/lab3/Zadanie4.java
@@ -1,10 +1,23 @@
-// Opracuj kod realizuj¹cy wczytywanie kolejnych liczb do momentu gdy podana zostanie wartoœæ 5. Wyœwietliæ iloczyn podanych liczb.
+// Opracuj kod realizujÄ…cy wczytywanie kolejnych liczb do momentu gdy podana zostanie wartoÅ›Ä‡ 5. WyÅ›wietliÄ‡ iloczyn podanych liczb.
 package lab3;
 
 public class Zadanie4
 {
 	public static void main(String[] args)
 	{
-		// TODO Auto-generated method stub
+		Vector<Integer> liczby = new Vector<>;
+		try (Scanner keyboard = new Scanner(System.in)
+		{	
+			do
+			{
+				do
+				{
+					liczby.add(keyboard.nextInt());
+				}
+				while (
+			}
+			while (liczby.lastElement() != 5)
+		}
+		     
 	}
 }
