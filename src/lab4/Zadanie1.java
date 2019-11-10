deae7ba5c9118f40cfef3a6e0e2c4833261a04a3
Hubert Gad <50676544+hubertgad@users.noreply.github.com>
2019-11-10 08:16:31 +0100

    Create Zadanie1.java

---
+++ src/lab4/Zadanie1.java
@@ -0,0 +1,22 @@
+package lab4;
+import static java.lang.System.*;
+
+public class Zadanie1 
+{
+	public static void main(String[] args) 
+	{
+		int[] tablica1 = new int[52];
+		int i = 99;
+		for (int j = 0; j < tablica1.length; j++)
+			tablica1[j]  = i++;
+		int[] tablica2 = tablica1;
+		out.print("Tablica nr 1: [ ");
+		for (int element : tablica1)
+			out.print(element + " ");
+		out.println("]");		
+		out.print("Tablica nr 2: [ ");
+		for (int element : tablica2)
+			out.print(element + " ");
+		out.println("]");			
+	}
+}
